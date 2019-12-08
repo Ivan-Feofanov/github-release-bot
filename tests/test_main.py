c42214d111d5f59da679d14bf00ae8064b7daa57
Ivan Feofanov <Ivan-Feofanov@users.noreply.github.com>
2019-12-08 16:28:43 +0500

    add prepare_markdown test (#1)
    * add prepare_markdown test
    
    * fix job name

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,9 +3,17 @@ from unittest.mock import patch
 from starlette import status
 from starlette.responses import Response
 
+from bot import prepare_markdown
+
 
 @patch('bot.bot.send_message')
 def test_main(mock, client, message_body):
     response: Response = client.post('/', json=message_body)
     assert response.status_code == status.HTTP_200_OK
     mock.assert_called_once()
+
+
+def test_prepare_markdown():
+    incoming_body = '###title\n\r* abc\r\n'
+    result = prepare_markdown(incoming_body)
+    assert result == '*title*\n\r- abc*\r\n'
