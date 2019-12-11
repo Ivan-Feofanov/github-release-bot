ca397a8ca873359c9482e2314643e7e1abfbfbff
Ivan Feofanov <feofanov@gmail.com>
2019-12-11 12:47:28 +0500

    move release_only to query params

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from starlette.background import BackgroundTasks
 from starlette.requests import Request
 from starlette.responses import Response
 
-import settings
 from bot import proceed_release
 from models import Body
 from utils import check_auth
@@ -16,13 +15,14 @@ app = FastAPI()  # noqa: pylint=invalid-name
 async def release(*,
                   body: Body,
                   chat_id: str = None,
+                  release_only: bool = False,
                   request: Request,
                   x_hub_signature: str = Header(''),
                   background_tasks: BackgroundTasks):
 
     check_auth(await request.body(), x_hub_signature)
 
-    if not (body.release.draft and settings.ONLY_PUBLISH):
+    if not (body.release.draft and release_only):
         background_tasks.add_task(proceed_release, body, chat_id)
 
     return Response(status_code=status.HTTP_200_OK)
--- settings.py
+++ settings.py
@@ -1,4 +1,3 @@
 import os
 
-ONLY_PUBLISH = bool(os.getenv('ONLY_PUBLISH'))
 SECRET_TOKEN = os.getenv('SECRET_TOKEN', 'not_secure')
